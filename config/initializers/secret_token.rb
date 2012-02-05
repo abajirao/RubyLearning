08ab415acf3629b08f36ae67661bd0cb0b423b89
Picsplore <dev@picsplore.com>
2012-02-05 09:05:31 -0600

    Lesson 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picsplore::Application.config.secret_token = '0adc06c9ab801a3936e83613657222a5fa7001db4013cee452854dfa4e6c376393c9c32b72372c9bbc85c316bbde3645e44c6dd9bdd89e4115a7b7d7d3d7ca37'
